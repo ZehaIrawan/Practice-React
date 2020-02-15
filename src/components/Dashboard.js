e300e9deb3af5dc658b959ef7801936e0b73116a
Zeha Irawan <jangkaringbumi@gmail.com>
2020-02-15 15:07:12 +0700

    practicing implicit return

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,14 +1,14 @@
 import React from 'react';
-import Rocket from './Rocket';
 import RocketClass from './RocketClass';
 
 const Dashboard = () => {
+  const rocketList = ['Falcon 9', 'Falcon Heavy', 'Falcon 1'];
   return (
     <div>
       <h1>Dashboard</h1>
-      <Rocket rocketName={'Falcon 9 "Full Thrust"'} />
-      <Rocket rocketName={'Falcon Heavy'} />
-      <Rocket rocketName={'Falcon 1'} />
+      {rocketList.map(rocket => (
+        <h2>{rocket}</h2>
+      ))}
       <div>---</div>
       <div>---</div>
       <div>---</div>
